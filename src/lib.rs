117f29ed12cff485e78a4e50fbc605ab0c825545
Piet Geursen <pietgeursen@gmail.com>
2021-11-17 17:30:04 +1300

    remove one allocation per row

--- src/lib.rs
+++ src/lib.rs
@@ -12,7 +12,7 @@ use rusqlite::{params, Connection, Error};
 #[derive(Snafu, Debug)]
 pub enum ReadDirError {
     EAcess { source: Error },
-    EBusy,
+    EBusy { source: Error },
 }
 
 // Implement a conversion from our error type to libc error codes.
@@ -33,19 +33,24 @@ pub extern "C" fn readdir(
     filler: fuse_fill_dir_t,
 ) -> c_int {
     // Convert the sqlite3 pointer to a Connection.
-    let connection = unsafe { Connection::from_handle(handle) }
-        .expect("todo: couldn't open Connection from handle");
+    let connection =
+        unsafe { Connection::from_handle(handle) }.expect("couldn't open Connection from handle");
 
     // Convert the path_ptr to a rust &str
     let path = unsafe { CStr::from_ptr(path_ptr) }
         .to_str()
-        .expect("todo: couldn't convert path to valid utf8 rust str");
+        .expect("couldn't convert path to valid utf8 rust str");
 
     // Run our query
     let result = readdir_(connection, path, |st| {
+        // We need to allocate a new CString here so that it's null terminated. (Rust strings are
+        // not null terminated)
+        // That's one more allocation per row than C has to do unfortunately.
         let s = CString::new(st).unwrap();
 
+        // Avoid calling a null function pointer
         if let Some(filler) = filler {
+            // Call our callback
             unsafe { filler(filler_buff, s.as_ptr(), null_mut(), 0) };
         }
     });
@@ -59,6 +64,8 @@ pub extern "C" fn readdir(
 }
 
 /// Internal method with no unsafe code that never panics.
+///
+/// Ok, it could panic if what's in the key column is not text but some other sql type.
 fn readdir_<F: FnMut(&str)>(
     connection: Connection,
     path: &str,
@@ -77,46 +84,25 @@ fn readdir_<F: FnMut(&str)>(
         .prepare_cached("select key from meta_data where key glob ?1 and key != ?2;")
         .context(EAcess)?;
 
-    // Actually do the query
-    let mut entries_iter = stmt
-        .query_map(params![glob, path], |row| {
-            let row_ref = row.get_ref(0)?.as_str()?;
-
-            let trimmed_result: String = (&row_ref[path.len() + 1..]).into();
-            Ok(trimmed_result)
-        })
-        .context(EAcess)?
-        .filter(|val| {
-            // Some results need to be filtered out
-            match val {
-                // Skip if grandchild etc
-                Ok(val) => !(val.is_empty() || val.contains("/")),
-                _ => true,
-            }
-        });
-
     // Part of the contract is that we always return these dirs
     cb(".");
     cb("..");
 
-    // If any loop returns an Err then we return that error immediately using `try_for_each`.
-    // This is slightly different from the c code. If any one result returns busy it will keep
-    // trying the next row. Which seems weird to me?
-    entries_iter.try_for_each(|entry| {
-        match entry {
-            Ok(entry) => Ok(cb(&entry)),
-            // We can't distinguish between SQLITE_BUSY or some other sqlite error because they're
-            // not exposed in this error type. We could probably get at them if we really cared.
-            // This just catches all errors and return EBusy
-            Err(_) => Err(ReadDirError::EBusy), //todo
-        }
-    })
-}
+    // Actually do the query
+    let mut rows = stmt.query(params![glob, path]).context(EAcess)?;
+    while let Some(row) = rows.next().context(EAcess)? {
+        // get_ref borrows data from sqlite. This avoids us allocating a new string.
+        let row_ref = row
+            .get_ref(0)
+            .context(EBusy)?
+            .as_str()
+            .expect("Expected key column to contain text but it is some other type");
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+        let trimmed_result = &row_ref[path.len() + 1..];
+
+        if !(trimmed_result.is_empty() || trimmed_result.contains("/")) {
+            cb(trimmed_result)
+        }
     }
+    Ok(())
 }
